5412ae16d1391585db1d1ae125fe4751c36e4bfa
shinyay <shinya.com@gmail.com>
2018-12-26 14:00:02 +0900

    Modify from Function to Supplier

--- src/main/java/io/pivotal/syanagihara/functions/NumberGenerator.java
+++ src/main/java/io/pivotal/syanagihara/functions/NumberGenerator.java
@@ -1,13 +1,21 @@
 package io.pivotal.syanagihara.functions;
 
 import java.util.Random;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class NumberGenerator implements Function<Integer, Integer> {
+public class NumberGenerator implements Supplier<Integer> {
+
+//    @Override
+//    public Integer apply(Integer bound) {
+//        return new Random().nextInt(bound) + 1;
+//    }
+
 
     @Override
-    public Integer apply(Integer bound) {
-        return new Random().nextInt(bound) + 1;
+    public Integer get() {
+        return new Random().nextInt(100) + 1;
     }
+
 }
